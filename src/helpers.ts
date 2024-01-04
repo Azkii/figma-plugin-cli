aeb11998ffeec38dec1461a87e1c700a98f88f73
Dawid <dawidpacanowski1@gmail.com>
2024-01-04 02:05:51 +0100

    feat: jsDocs for copyContentToDirectory added

--- src/helpers.ts
+++ src/helpers.ts
@@ -26,6 +26,27 @@ interface Options {
   renameFiles?: { [oldName: string]: string };
 }
 
+/**
+ * Options for customizing the behavior of the copyContentToDirectory function.
+ * @property {Object.<string, string>} [renameFiles] - A mapping of old file names to new file names for renaming files during the copy process.
+ *   This should be structured like: { [oldName: string]: string }, where each key represents the old file name and the corresponding value is the new file name to replace it.
+ */
+
+/**
+ * Recursively copies the content of a directory to another directory, with optional file renaming.
+ * @param {string} copyFrom - The source directory to copy from.
+ * @param {string} copyInto - The target directory to copy into.
+ * @param {Options} [options={}] - Additional options for customizing the copy process.
+ */
+
+/**
+ * Example usage:
+ * @example
+ * copyContentToDirectory(templateDir, targetDir, {
+ *   renameFiles: { _gitignore: ".gitignore" },
+ * });
+ */
+
 export const copyContentToDirectory = (
   copyFrom: string,
   copyInto: string,
