df9bb4392d35922e3f699c612d46dd26efb4b348
Dawid <dawidpacanowski1@gmail.com>
2024-01-06 22:53:23 +0100

    feat: svelte-ts template added

---
+++ templates/svelte-ts/ui-app/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let name = "Figma";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+</main>
+
+<style lang="scss">
+  main {
+    font-family: sans-serif;
+    text-align: center;
+
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4rem;
+      font-weight: 200;
+    }
+  }
+</style>
