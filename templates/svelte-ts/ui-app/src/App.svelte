b9d64163d0f6a780eb7ba035cde25711f100e2af
Dawid Pacanowski <dawidpacanowski1@gmail.com>
2024-01-08 23:33:44 +0100

    feat: svelte ts template screen added

--- templates/svelte-ts/ui-app/src/App.svelte
+++ templates/svelte-ts/ui-app/src/App.svelte
@@ -1,21 +1,62 @@
 <script lang="ts">
-  export let name = "Figma";
+  export let name = "Svelte";
+  import logoSrc from "./assets/logo.png";
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
+<main class="main">
+  <img
+    width="160"
+    height="160"
+    src={logoSrc}
+    alt="javascript logo"
+    class="logo"
+  />
+  <div class="content">
+    <h1 class="title">
+      {name}&nbsp;<span class="logo-text">TS</span> template
+    </h1>
+    <p class="sub-title">Figma Plugin CLI</p>
+  </div>
 </main>
 
 <style lang="scss">
-  main {
-    font-family: sans-serif;
-    text-align: center;
+  .main {
+    display: grid;
+    place-content: center;
+    justify-items: center;
+    gap: 24px;
 
-    h1 {
-      color: #ff3e00;
-      text-transform: uppercase;
-      font-size: 4rem;
-      font-weight: 200;
-    }
+    height: 100%;
+    width: 100%;
+    background-color: var(--figma-color-bg, #2c2c2c);
+
+    text-align: center;
+    font-family: "Inter", sans-serif;
+    color: var(--figma-color-text, #ffffff);
+  }
+
+  .content {
+    display: grid;
+    place-content: center;
+    gap: 16px;
+  }
+
+  .logo {
+    width: 160px;
+    height: 160px;
+  }
+
+  .title {
+    font-size: 3rem;
+    font-weight: 500;
+  }
+
+  .logo-text {
+    color: #358ef1;
+  }
+
+  .sub-title {
+    font-size: 1.5rem;
+    color: var(--figma-color-text-secondary, #ffffffb2);
   }
 </style>
